553c6c5f0a437774faf780f5348bf846c4a0fe2a
mtdemircan <72382983+mtdemircan@users.noreply.github.com>
2022-06-10 19:09:26 +0300

    terrain added

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    private double damage;
+    private double length;
+    private double damage_angle;
+
+    public void Hit()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {   
+
+        }
+    }
+    
+}
